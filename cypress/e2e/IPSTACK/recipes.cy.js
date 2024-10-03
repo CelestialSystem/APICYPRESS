19ae247b462309fdacacc9953a28980950add825
abhishek9celestial <abhishek98.celestialsys@gmail.com>
2024-10-03 21:05:38 +0530

    Ipstack changes added

--- cypress/e2e/IPSTACK/recipes.cy.js
+++ cypress/e2e/IPSTACK/recipes.cy.js
@@ -1,31 +1,37 @@
-describe('Contact Form Page', () => {
+import { IPSTACK_BASE_URL } from '../../resources/data';
+
+describe('Ipstack Recipe page', () => {
     before(() => {
-        // Visit the contact form page before running any tests
-        cy.visit('https://ipstack.com/');
+        cy.visit(IPSTACK_BASE_URL);
     });
 
-    it('1. Test to naviagate to the FAQs page', () => {
+    it('1. Test to naviagate to the "Recipes" page', () => {
         cy.AcceptCookies();
-        cy.get('.header > .container > ul > :nth-child(3) > a').click();
-        cy.visit('https://ipstack.com/recipe');
+        cy.get('.custom-container > :nth-child(3) > :nth-child(6) > a').click();
+        cy.navigateUrlwithCookies('https://ipstack.com/recipe');
     });
-    it('2. Test to show the Recipes page', ()=> {
-        cy.get(':nth-child(1) > .feature-blog-text > :nth-child(1) > .post_title').should('contain.text', 'Ipstack Fraud Detection Recipe');
+
+    it('2. Test to show the text "Ipstack Fraud Detection Recipe" is present', () => {
+        cy.componentVisiblityCheck(':nth-child(1) > .feature-blog-text > :nth-child(1) > .post_title', 'Ipstack Fraud Detection Recipe');
     });
-    it('3. Test to check the Recipes inner text available or not', ()=> {
-        cy.get(':nth-child(2) > .feature-blog-text > :nth-child(1) > .post_title').should('contain.text', 'Leveraging IP Stack for Enhanced Technical Support: An Overview')
+
+    it('3. Test to check the text "Leveraging IP Stack for Enhanced Technical Support: An Overview" is present', () => {
+        cy.componentVisiblityCheck(':nth-child(2) > .feature-blog-text > :nth-child(1) > .post_title', 'Leveraging IP Stack for Enhanced Technical Support: An Overview')
     });
-    
-    it('4. Test to check for the sidebar recipes text', () => {
-        cy.get('.blog-sidebar-content-post').should('contain.text', 'Recipes')
-    })
-    it('5. Test to check for the home button recipes', () => {
-        cy.get('p > [href="/"]').should('contain.text', 'Home');
-    })
-    it('6. Test to check for the receipes button', () => {
-    cy.get('.posts_text').click();
-    })
-    it('7. Test to click on the footer link recipes', () => {
+
+    it('4. Test to check for the sidebar "Recipes" text', () => {
+        cy.componentVisiblityCheck('.blog-sidebar-content-post', 'Recipes')
+    });
+
+    it('5. Test to check for the "Home" button recipes', () => {
+        cy.componentVisiblityCheck('p > [href="/"]', 'Home');
+    });
+
+    it('6. Test to check for the "Receipes" button', () => {
+        cy.get('.posts_text').click();
+    });
+
+    it('7. Test to click on the footer link "Recipes"', () => {
         cy.get('.custom-container > :nth-child(3) > :nth-child(6) > a').click();
     });
 });
--- cypress/e2e/IPSTACK/status.cy.js
+++ cypress/e2e/IPSTACK/status.cy.js
@@ -1,116 +1,121 @@
-describe('Test Cases for IPstack status page.', () => {
+describe('Ipstack Status page.', () => {
     before(() => {
         cy.visit('https://status.ipstack.com/');
     });
 
-    it('1.Test to Accept Cookies if not already accepted', () => {
+    it('1.Test to "Accept Cookies" if not already accepted', () => {
         cy.AcceptCookies();
-        // Check if the "Accept Cookies" button is visible or exists
-        cy.get('body').then((body) => {
-            if (body.find('.lcc-modal--alert').length > 0) {
-                cy.get('.lcc-modal__actions > .js-lcc-accept').click(); // Click the button if it exists
-                cy.log('Cookies accepted.');
-            } else {
-                cy.log('Cookies already accepted or not present.');
-            }
+    });
+
+    it('2.Test to check that the page title "Service status" is present', () => {
+        cy.document().should((doc) => {
+            expect(doc.readyState).to.equal('complete');
         });
+        cy.checkTextVisibility('Service status');
     });
 
-    it('2.Test to check that the page title contains "Service status"', () => {
-        cy.get('.uk-margin-remove').should('be.visible', 'contain.text', 'Service status');
+    it('3. Test to check that the status "All systems operational" is present', () => {
+        cy.checkTextVisibility('All systems operational');
     });
 
-    it('3. Test to check that the status "All systems operational" is visible', () => {
-        cy.get('.psp-main-status').contains('All systems operational').should('be.visible', 'contain', 'All systems operational');
+    it('4. Test to check the "Uptime" graph text is present', () => {
+        cy.componentVisiblityCheck('.uk-flex-between.uk-flex-middle > .uk-h3', 'Uptime');
     });
 
-    it('4. Test to check the uptime graph text is visible', () => {
-        cy.get('.uk-flex-between.uk-flex-middle > .uk-h3').should('be.visible', 'contain.text', 'Uptime');
+    it('5. Test to check the "Calender View" button', () => {
+        cy.componentVisiblityCheck('.psp-calendar-link')
     });
 
-    it('5. Test to check the Calender button', () => {
-        cy.get('.psp-calendar-link').should('be.visible');
+    it('6. Test to check the "api.ipstack.com" text is present', () => {
+        cy.componentVisiblityCheck('.psp-monitor-name','api.ipstack.com');
     });
 
-    it('6. Test to check the API stak.com text is there or not', () => {
-        cy.get('.psp-monitor-name').should('be.visible', 'contain.text', 'api.ipstack.com');
+    it('7. Test to check the overall uptime for the last 24 hours is 100.000%', () => {
+        cy.componentVisiblityCheck('.uk-first-column > .uk-h4', '100.000%');
     });
 
-    it('7. Test to check the overall uptime for the last 24 hours is 100%', () => {
-        cy.get('.uk-first-column > .uk-h4').should('contain.text', '100.000%');
-    });
-
-    it('8. Test to check the overall uptime for the last 7 days is 100%', () => {
-        cy.get(':nth-child(2) > .uk-h4').should('contain.text', '100.000%');
+    it('8. Test to check the overall uptime for the last 7 days is 100.000%', () => {
+        cy.componentVisiblityCheck(':nth-child(2) > .uk-h4', '100.000%');
     });
 
     it('9. Test to check the overall uptime for the last 30 days is 100.000%', () => {
-        cy.get(':nth-child(3) > .uk-h4').should('contain.text', '100.000%');
+        cy.componentVisiblityCheck(':nth-child(3) > .uk-h4', '100.000%');
     });
 
     it('10. Test to check the overall uptime for the last 90 days is 99.995%', () => {
-        cy.get(':nth-child(4) > .uk-h4').should('contain.text', '99.995%');
+        cy.componentVisiblityCheck(':nth-child(4) > .uk-h4', '99.995%');
     });
 
-    it('11. Test to check the "Status updates" section is visible', () => {
-        cy.get('.anouncement-header > .uk-h3').should('be.visible', 'contain.text', 'Status updates');
+    it('11. Test to check the "Status updates" section is present', () => {
+        cy.componentVisiblityCheck('.anouncement-header > .uk-h3', 'Status updates');
     });
 
-    it('12. Test to check the "Status update history" link is visible and clickable', () => {
-        cy.get('.announcement-empty > .psp-history-link').click();
-        cy.wait(1000);
-        cy.get('.uk-flex-between > .uk-margin-remove').should('be.visible', 'contain.text', 'Status update history');
+    it('12. Test to check the "Status update history" link is present and clickable', () => {
+        cy.get('.announcement-empty > .psp-history-link').click({force: true});
+        cy.document().should((doc) => {
+            expect(doc.readyState).to.equal('complete');
+        });
+        cy.componentVisiblityCheck('.uk-flex-between > .uk-margin-remove', 'Status update history');
         cy.go('back');
-        cy.wait(500);
     });
 
-    it('13. Test to check the "Full screen mode" button is visible', () => {
-        cy.get('.enter-fullscreen').should('be.visible');
+    it('13. Test to check the "Full screen mode" button is present', () => {
+        cy.document().should((doc) => {
+            expect(doc.readyState).to.equal('complete');
+        });
+        cy.get('.enter-fullscreen').scrollIntoView();
+        cy.componentVisiblityCheck('.enter-fullscreen');
     });
 
-    it('14. Test to check the "Alert Sound off" button is visible', () => {
-        cy.get('.toggle-notif').should('be.visible');
+    it('14. Test to check the "Alert Sound off" button is present', () => {
+        cy.componentVisiblityCheck('.toggle-notif');
     });
 
-    it('15. Test to check the "Terms of Service" link is visible and clickable', () => {
-        cy.get('[href="https://uptimerobot.com/terms"]').contains('Terms of Service').should('be.visible');
+    it('15. Test to check the "Terms of Service" link is present and clickable', () => {
+        cy.componentVisiblityCheck('[href="https://uptimerobot.com/terms"]');
     });
 
-    it('16. Test to check the Calender View button will open the calender or not', () => {
-        cy.get('.psp-calendar-link').click();
+    it('16. Test to check the "Calender View" button will open the calender ', () => {
+        cy.get('.psp-calendar-link').click({force: true});
+        cy.document().should((doc) => {
+            expect(doc.readyState).to.equal('complete');
+        });
         cy.visit('https://status.ipstack.com/780159446/calendar');
     });
 
-    it('17. Test to check "api.ipstack.com history logs" text is there or not', () => {
-        cy.get('.monitor-name').should('contain.text', 'api.ipstack.com history logs');
+    it('17. Test to check "api.ipstack.com history logs" text is present', () => {
+        cy.document().should((doc) => {
+            expect(doc.readyState).to.equal('complete');
+        });
+        cy.componentVisiblityCheck('.monitor-name', 'api.ipstack.com history logs');
     });
 
-    it('18. Test to check the choose container is there or not', () => {
-        cy.get('#select2-calendar-monitor-chooser-container').should('be.visible', 'contain.text', 'api.ipstack.com');
+    it('18. Test to check the choose container is present', () => {
+        cy.componentVisiblityCheck('#select2-calendar-monitor-chooser-container', 'api.ipstack.com');
     });
 
-    it('19. Test to check for the selection dropdown clickable or not', () => {
-        cy.get('.select2-selection__arrow').click();
+    it('19. Test to check for the selection dropdown clickable ', () => {
+        cy.get('.select2-selection__arrow').click({force: true});
     });
 
-    it('20.Test to check the navigation calender availble or not', () => {
-        cy.get('.psp-calendar-nav').should('be.visible');
+    it('20.Test to check the navigation calender is present', () => {
+        cy.componentVisiblityCheck('.psp-calendar-nav');
     });
 
     it('21. Test to check the previous calender arrow button should be clickable', () => {
-        cy.get('.calendar-prev > .icon').click();
+        cy.componentVisiblityCheck('.calendar-prev > .icon').click({force: true});
     });
 
-    it('22. Test to check the first date column is visible or not', () => {
-        cy.get('.uk-first-column > table').should('be.visible');
+    it('22. Test to check the first date column is present ', () => {
+        cy.componentVisiblityCheck('.uk-first-column > table');
     });
 
-    it('23. Test to check the second date column is visible or not', () => {
-        cy.get(':nth-child(2) > table').should('be.visible');
+    it('23. Test to check the second date column is present ', () => {
+        cy.componentVisiblityCheck(':nth-child(2) > table');
     });
 
-    it('24. Test to check the third date column is avaible or not', () => {
-        cy.get(':nth-child(1) > table').should('be.visible');
+    it('24. Test to check the third date column is present ', () => {
+        cy.componentVisiblityCheck(':nth-child(1) > table');
         cy.go('back');
     });
 });
