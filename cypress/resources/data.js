4a4c64155a9766deb2125678806101fa9b9af39a
AngadCel <105632526+AngadCel@users.noreply.github.com>
2024-10-14 20:48:46 +0530

    written ResumeParserAPI test cases
    written ResumeParserAPI test cases

--- cypress/resources/data.js
+++ cypress/resources/data.js
@@ -16,3 +16,4 @@ export const EMAIL_VERIFICATION_BASE_URL = `${APILAYER_BASE_URL}/marketplace/ema
 export const BANK_DATA_BASE_URL = "https://www.bankdataapi.com/";
 export const BINCHECKER_BASE_URL = `${APILAYER_BASE_URL}/marketplace/bincheck-api`;
 export const IPTOLOCATIONAPI_BASE_URL = `${APILAYER_BASE_URL}/marketplace/ip_to_location-api`;
+export const RESUME_PARSER_API = `${APILAYER_BASE_URL}/marketplace/resume_parser-api`;
