fd57a8d0dff2214063bf398c1925cedd89db4c3e
sarthak492 <105632422+sarthak492@users.noreply.github.com>
2024-10-14 15:55:05 +0530

    updated blog page

--- cypress/e2e/NumVerify/blog.cy.js
+++ cypress/e2e/NumVerify/blog.cy.js
@@ -14,10 +14,29 @@ describe('NumVerify Blogs Page', () => {
         cy.url().should('eq', 'https://blog.numverify.com/')
     });
 
-    it('2. Tets to check "Recent Post" section is presentx', () => {
-        cy.componentVisiblityCheck('.et_pb_text_0', '\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\tRECENT POST\n\t\t\t')
+    it('3. test to check "Recent Post" section is present', () => {
+        cy.componentVisiblityCheck('.et_pb_text_0', '\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\tRECENT POST\n\t\t\t');
     });
 
-    
+    it('4. test to check "Top Post" section is present', () => {
+        cy.componentVisiblityCheck('.et_pb_text_1', '\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\tTOP POST\n\t\t\t');
+    });
+
+    it('5. test to check "pagination" section is present', () => {
+        cy.componentVisiblityCheck('.wp-pagenavi');
+    });
+
+    it('6. test to check recent blogs should be present in blog view', () => {
+        cy.get(':nth-child(1) > .wp-block-latest-posts__post-title')
+      .invoke('text')
+      .then((firstText) => {
+        cy.get('#post-15503 > .entry-title > a')
+          .invoke('text')
+          .then((secondText) => {
+            expect(firstText.trim()).to.equal(secondText.trim());
+          });
+      });
+    });
+
 });
 
