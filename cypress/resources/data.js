abe55f0331276f514bbab6cb9ccccee292067e01
SinchanaGoudar4 <sinchana.n@celestialsys.com>
2024-10-14 19:13:28 +0530

    Added the test cases for spam check api
    Added the test cases for spam check api it has the home, pricing, api info and documentation page changes

--- cypress/resources/data.js
+++ cypress/resources/data.js
@@ -18,3 +18,5 @@ export const EMAIL_VERIFICATION_BASE_URL = `${APILAYER_BASE_URL}/marketplace/ema
 export const BANK_DATA_BASE_URL = "https://www.bankdataapi.com/";
 export const BINCHECKER_BASE_URL = `${APILAYER_BASE_URL}/marketplace/bincheck-api`;
 export const IPTOLOCATIONAPI_BASE_URL = `${APILAYER_BASE_URL}/marketplace/ip_to_location-api`;
+export const SPAM_CHECK_API_BASE_URL = `${APILAYER_BASE_URL}/marketplace/spamchecker-api`;
+
