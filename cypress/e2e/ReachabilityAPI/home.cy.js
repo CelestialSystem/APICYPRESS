45f6d76ee19bd848098a4be3e47d21dbd689df0c
AngadCel <105632526+AngadCel@users.noreply.github.com>
2024-10-16 19:02:49 +0530

    test cases for Reachability API
    test cases for Reachability API

---
+++ cypress/e2e/ReachabilityAPI/home.cy.js
@@ -0,0 +1,91 @@
+import { REACHABILITY_API_BASE_URL } from '../../resources/data';
+
+describe('Reachability API Home page', () => {
+    before(() => {
+        cy.visit(REACHABILITY_API_BASE_URL);
+    });
+
+    it('1. Test to "Accept Cookies" if not already accepted', () => {
+        // Check if the "Accept Cookies" button is present or exists
+        cy.AcceptCookies();
+    });
+
+    it('2. Test to check "Reachability API" title is present', () => {
+        cy.componentVisiblityCheck('.h2', 'Reachability API');
+    });
+
+    it('3. Test to check "Subscribe for free" button is present', () => {
+        cy.componentVisiblityCheck('#subscribeButton', 'Subscribe for Free');
+    });
+
+    it('4. Test to check "Live Demo" button is present and clickable', () => {
+        // Check if the "Live Demo" button is visible
+        cy.componentVisiblityCheck('.show-code', 'Live Demo');
+
+        // Click the "Live Demo" button
+        cy.get('.show-code').click();
+
+        // Verify that the sidebar content is visible after clicking the button
+        cy.componentVisiblityCheck('.sidebar-content');
+
+        // Click on the body to close any open elements
+        cy.get('body').click(0, 0);
+    });
+
+    it('5. Test to check code example  is present', () => {
+        cy.componentVisiblityCheck('.code-response');
+    });
+
+    it('6. Test to check "Pricing tab"  is present', () => {
+        cy.componentVisiblityCheck('#pricing-tab', 'Pricing');
+    });
+
+    it('7. Test to check "API info" tab  is present', () => {
+        cy.componentVisiblityCheck('#details-tab', 'API info');
+    });
+
+    it('8. Test to check "Documentation" tab  is present', () => {
+        cy.componentVisiblityCheck('#documentation-tab', 'Documentation');
+    });
+
+    it('9. Test to check "Related Products" container is present', () => {
+        cy.componentVisiblityCheck('.h1', 'Related Products');
+    });
+
+    it('10. Test to check footer is present', () => {
+        cy.componentVisiblityCheck('footer.bg-primary');
+    });
+
+    it('11. Test to check on the "Reachability API" description is present', () => {
+        // Check if the description is visible
+        cy.componentVisiblityCheck('.col-9 > div.mb-5', 'Check reachability of any site/webpage from any country in the world.');
+    });
+
+    it('12. Test to check the "check the documentation" link is present and clickable', () => {
+        // Check if the "check the documentation" link is visible
+        cy.componentVisiblityCheck('p > a');
+
+        // Click the "check the documentation" link
+        cy.get('a[href="javascript:;"]').contains('check the documentation.').click();
+
+        // Verify that the expected text is visible after the link is clicked
+        cy.checkTextVisibility('Reachability API Reference');
+    });
+
+    it('13. Test to check APILayer link, logo, and review stars', () => {
+        // Check if the APILayer link is present and visible
+        cy.get('.media.align-items-center[href="/"]')
+            .should('exist')
+            .and('be.visible');
+
+        // Verify the APILayer text is present
+        cy.get('.media-body.font-size-1.text-body')
+            .contains('APILayer')
+            .should('exist')
+            .and('be.visible');
+
+        // Check if 5 review stars are present and visible
+        cy.get('.d-flex.align-items-center li img[alt="Review rating"]')
+            .and('be.visible');
+    });
+});
---
+++ cypress/e2e/ReachabilityAPI/pricing.cy.js
@@ -0,0 +1,170 @@
+import { REACHABILITY_API_BASE_URL } from '../../resources/data';
+
+describe('Reachability API Pricing page', () => {
+    before(() => {
+        cy.visit(REACHABILITY_API_BASE_URL);
+    });
+
+    it('1. Test to "Accept Cookies" if not already accepted', () => {
+        // Check if the "Accept Cookies" button is visible or exists
+        cy.AcceptCookies();
+    });
+
+    it('2. Verify the "Reachability API" title is visible', () => {
+        cy.get('h1.h2.mb-3.text-primary')
+            .should('be.visible') // Ensure the element is visible
+            .and('contain.text', 'Reachability API'); // Verify the text content
+    });
+
+    // Test to check visibility of the "Free Plan" title and its price
+    it('3. Test to check "Free Plan" title and price are present', () => {
+        // Verify "Free Plan" title visibility and content
+        cy.get('.card-header .d-block.h3')
+            .should('be.visible')
+            .and('contain.text', 'Free Plan');
+
+        // Verify "Free Plan" price visibility and content
+        cy.get('.plan-price.font-size-4.font-weight-bold.text-primary')
+            .should('be.visible')
+            .and('contain.text', '$0.00');
+
+        // Check if the card body is present and visible
+        cy.get('.card-body.py-0.px-3')
+            .should('exist')
+            .and('be.visible');
+
+        // Check for "100 Requests / Monthly" text
+        cy.get('.card-body .media-body')
+            .contains('3,000 Requests / Monthly')
+            .should('exist')
+            .and('be.visible');
+
+        // Check for "Free for Lifetime" text
+        cy.get('.card-body .media-body')
+            .contains('Free for Lifetime')
+            .should('exist')
+            .and('be.visible');
+
+        // Check for "No Credit Card Required" text
+        cy.get('.card-body .media-body')
+            .contains('No Credit Card Required')
+            .should('exist')
+            .and('be.visible');
+    });
+
+    // Test to verify the "Subscribe" button visibility and click functionality
+    it('4. Test to click on "Subscribe" button for Free Plan', () => {
+        // Verify that the button is visible and contains the text "Subscribe"
+        cy.get('a.btn.btn-success.btn-block.transition-3d-hover.js-hs-unfold-invoker')
+            .should('be.visible')
+            .and('contain.text', 'Subscribe')
+            .click(); // Perform the click action
+
+        // Check if the sidebar content becomes visible after the click
+        cy.get('#sidebarContent')
+            .should('be.visible');
+
+        cy.get('body').click(0, 0); // Click at the top-left corner of the body to close any opened dropdowns
+    });
+
+    // Test to check the visibility of the "Starter Plan" title and price
+    it('5. Test to check "Starter Plan" title and price and other contents are present', () => {
+        // Verify the "Starter Plan" title is visible and contains the correct text
+        cy.get('span.d-block.h3')
+            .should('be.visible')
+            .and('contain.text', 'Starter Plan');
+
+        // Verify the price of the "Starter Plan" is visible and contains the correct amount
+        cy.get('.plan-price.font-size-4.font-weight-bold.text-primary')
+            .should('be.visible')  // Ensure the price element is visible
+            .and('contain.text', '$3.99'); // Verify the price text is correct
+
+        // Check if the card body is present and visible
+        cy.get('.card-body.py-0.px-3')
+            .should('exist')
+            .and('be.visible');
+
+        // Verify "1,500 Requests / Monthly" text is present and visible
+        cy.get('.card-body .media-body')
+            .contains('15,000 Requests / Monthly')
+            .should('exist')
+            .and('be.visible');
+
+        // Verify "Standard Support" text is present and visible
+        cy.get('.card-body .media-body')
+            .contains('Standard Support')
+            .should('exist')
+            .and('be.visible');
+
+        // Check for the "Most Popular" badge and ensure it is visible
+        cy.get('.badge.badge-secondary.rounded-0.position-absolute.w-100')
+            .should('be.visible')  // Ensure the badge is visible
+            .and('contain.text', 'MOST POPULAR'); // Verify the correct text is displayed
+    });
+
+    it('6. Test to click on "Subscribe" button for starter plan', () => {
+        cy.get('a.btn.btn-success.btn-block.transition-3d-hover.js-hs-unfold-invoker') // Select the <a> element
+            .should('be.visible') // Ensure the button is visible
+            .and('contain.text', 'Subscribe') // Verify the text is correct
+            .click(); // Perform the click action
+
+        // Check for the expected text in the sidebar
+        cy.get('.lead').should('contain.text', 'Sign in to APILayer');
+
+        // Optional: Close the sidebar or click outside
+        cy.get('body').click(0, 0); // Click at the top-left corner of the body to close any opened dropdowns
+    });
+
+    it('7. Test to check "Pro Plan" details are and other contents present', () => {
+        // Check if the "Pro Plan" title is visible
+        cy.componentVisiblityCheck('.card-header .h3', 'Pro Plan');
+
+        // Check if the price for "Pro Plan" is visible
+        cy.componentVisiblityCheck('.plan-price', '$7.99');
+
+        // Check the first feature of "Pro Plan"
+        cy.componentVisiblityCheck('.card .media-body', '600,000\nRequests / Monthly');
+
+        // Check the second feature of "Pro Plan"
+        cy.componentVisiblityCheck('.card .media-body', 'Standard Support');
+    });
+
+    it('8. Test to check "Custom Plan" details are present', () => {
+        // Check if the "Custom Plan" title is visible
+        cy.componentVisiblityCheck('.card-header .h3', 'Custom Plan');
+
+        // Check if "Volume" text is visible
+        cy.componentVisiblityCheck('.font-size-4.text-primary', 'Volume');
+
+        // Check if the feature for "Custom Plan" is visible
+        cy.componentVisiblityCheck('.card-body .media-body', 'Any requests volume you need');
+
+        // Check if the "Contact Us" button is present and clickable
+        cy.get('.btn.btn-outline-primary')
+            .should('be.visible')
+            .and('contain.text', 'Contact\nUs');
+    });
+
+    it('9. Test to check "Contact Us" button text for Custom Plan is present and verify that the form opens on click', () => {
+        // Check if the "Contact Us" link is visible and contains the correct text
+        cy.componentVisiblityCheck('.btn.btn-outline-primary', 'Contact\nUs');
+
+        // Ensure the button is visible before clicking
+        cy.get('.card-header:has(.h3:contains("Custom Plan"))')
+            .find('a.btn.btn-outline-primary')
+            .should('be.visible')
+            .and('contain.text', 'Contact\nUs')
+            .click();
+
+        // Verify that the document is fully loaded
+        cy.document().should((doc) => {
+            expect(doc.readyState).to.equal('complete');
+        });
+
+        // Check if the correct header is visible in the new page/form
+        cy.componentVisiblityCheck('h1', 'Contact Sales & Customer Support');
+
+        // Navigate back to the main page
+        cy.navigateUrlwithCookies(REACHABILITY_API_BASE_URL);
+    });
+});
