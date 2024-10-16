7d1f93e5d8931d5c96b441777ddd4cdb0a4d701f
AngadCel <105632526+AngadCel@users.noreply.github.com>
2024-10-16 16:25:28 +0530

    test cases for Image Optimizer API
    test cases for Image Optimizer API

---
+++ cypress/e2e/ImageOptimizerAPI/documentation.cy.js
@@ -0,0 +1,243 @@
+import { IMAGE_OPTIMIZER_API, DEVELOPMENT_QUICKSTART_GUID } from '../../resources/data';
+
+describe('Image Optimizer API Documentation page', () => {
+    before(() => {
+        cy.visit(IMAGE_OPTIMIZER_API);
+    });
+
+    it('1. Test to "Accept Cookies" if not already accepted', () => {
+        // Check if the "Accept Cookies" button is visible or exists
+        cy.AcceptCookies();
+    });
+
+    it('2. Test to check "Documentation" tab visibility and Test to click "Documentation" tab', () => {
+        cy.componentVisiblityCheck('span', 'Documentation');
+        cy.intercept('GET', '/marketplace/image_optimizer-api/tabs/api_docs').as('apiRequest');
+        cy.contains('span', 'Documentation').click();
+        cy.wait('@apiRequest');
+    });
+
+    it('3. Test to check "Image Optimizer API Reference" title is present', () => {
+        cy.checkTextVisibility('Image Optimizer API Reference');
+    });
+
+    it('4. Test to check "development quickstart guide" link visibility and click behavior', () => {
+        cy.componentVisiblityCheck(`a[href="${DEVELOPMENT_QUICKSTART_GUID}"]`, 'development quickstart guide.');
+
+        cy.intercept('GET', DEVELOPMENT_QUICKSTART_GUID).as('quickstartRequest');
+
+        // Click the link and wait for the request
+        cy.get(`a[href="${DEVELOPMENT_QUICKSTART_GUID}"]`).invoke('removeAttr', 'target').click();
+        cy.wait('@quickstartRequest');
+
+        cy.get('h1.h2') // Select the <h1> element with the class 'h2'
+            .should('be.visible') // Assert that it is visible
+            .and('contain.text', 'Getting Started'); // Assert that it contains the text "Getting Started"
+
+        cy.navigateUrlwithCookies(IMAGE_OPTIMIZER_API);
+        cy.contains('span', 'Documentation').should('be.visible').click();
+    });
+
+    it('5. Test to check "Contents:" span visibility and content', () => {
+        cy.componentVisiblityCheck('span.text-muted.font-weight-bold', 'Contents:');
+    });
+
+    it('6. Test to check "Authentication" link visibility and content', () => {
+        cy.componentVisiblityCheck('a[href="#authentication"]', 'Authentication');
+    });
+
+    it('7. Test to click on "Authentication" hyperlink and check "Authentication" title is present', () => {
+        // Use a more resilient selector to find the "Authentication" link
+        cy.contains('a', 'Authentication').click();
+
+        // Check if the "Authentication" title is visible
+        cy.get('h4.mb-4.mt-4')
+            .should('be.visible') // Ensure the element is visible
+            .and('contain.text', 'Authentication'); // Verify the correct text is present
+    });
+
+    it('8. Test to click on "Accounts page" hyperlink and navigate back to main page', () => {
+        // Click the "Accounts page" link, ensuring it opens in the same tab
+        cy.contains('a', 'Accounts page').invoke('removeAttr', 'target').click();
+
+        // Check if the "API Marketplace" header is visible
+        cy.get('h1 a.text-dark')
+            .should('be.visible')
+            .and('contain.text', 'API Marketplace');
+
+        // Check if the introductory paragraph is present
+        cy.get('p').should('be.visible')
+            .and('contain.text', 'Discover, integrate, and empower your applications with our API marketplace.');
+
+        // Navigate back to the main page with cookies preserved
+        cy.navigateUrlwithCookies(IMAGE_OPTIMIZER_API);
+
+        // Perform further actions on the main page
+        cy.contains('span', 'Documentation').should('be.visible').click();
+    });
+
+    it('9. Test to check and click on "Endpoints" hyperlink, then verify title is present', () => {
+        // Check if the "Endpoints" link is visible and click it
+        cy.contains('a', 'Endpoints')
+            .should('be.visible') // Assert the link is visible
+            .click(); // Click the link
+
+        // Check for the title or expected content after clicking
+        cy.get('h4.mb-4.mt-4')
+            .should('be.visible') // Ensure the element is visible
+            .and('contain.text', 'Endpoints'); // Verify the correct text is present
+    });
+
+    it('10. Test to check and click on "Rate Limiting" hyperlink, then verify title is present', () => {
+        // Use a robust selector to find the "Rate Limiting" link
+        cy.contains('a', 'Rate Limiting')
+            .should('be.visible') // Check if the link is visible
+            .click(); // Click the "Rate Limiting" link
+
+        // Check for the title or expected content after clicking
+        cy.get('h4.mb-4.mt-4')
+            .should('be.visible') // Ensure the element is visible
+            .and('contain.text', 'Rate Limiting'); // Verify the correct text is present
+    });
+
+    it('11. Test to check the visibility of "Support unit" link, click on it, and navigate back to main page', () => {
+        // Check if the "Support unit" link is visible
+        cy.componentVisiblityCheck('a', 'support unit'); // Check visibility and text
+
+        // Click the "Support unit" link
+        cy.contains('a', 'support unit').click();
+
+        // Navigate back to the main page
+        cy.navigateUrlwithCookies(IMAGE_OPTIMIZER_API);
+
+        cy.contains('span', 'Documentation').should('be.visible').click();
+    });
+
+    it('12. Test to check and click on "Error Codes" hyperlink, then verify title is present', () => {
+        // Check if the "Error Codes" link is visible
+        cy.contains('a', 'Error Codes').should('be.visible'); // Assert the link is visible
+
+        // Click the "Error Codes" link
+        cy.contains('a', 'Error Codes').click();
+
+        // Check for the title or expected content after clicking
+        cy.get('h4.mb-4.mt-4')
+            .should('be.visible') // Ensure the element is visible
+            .and('contain.text', 'Error Codes'); // Verify the correct text is present
+    });
+
+    it('13. Test to check "Contact for support" link visibility and click', () => {
+        // Check if the "Contact for support" link is visible
+        cy.contains('a', 'contact for support').should('be.visible').click();
+
+        // Navigate back to the main page
+        cy.navigateUrlwithCookies(IMAGE_OPTIMIZER_API);
+
+        cy.contains('span', 'Documentation').should('be.visible').click();
+    });
+
+    it('14. Verify the "GET /url" endpoint content and click "Subscribe for Free"', () => {
+        // Verify the GET /url heading is present and visible
+        cy.get('#endpointHeading1 button')
+            .find('i.fas.fa-angle-right')
+            .should('be.visible');
+
+        cy.get('#endpointHeading2 > .mb-0 > .btn').click();
+        // Check if the endpoint description is correct
+        cy.get('#endpointCollapse1 .card-body p')
+            .contains('Optimizes and reduces file size of the given URL.')
+            .should('be.visible');
+
+        // Verify the Parameters section
+        cy.get('#endpointCollapse1 .card-body h6')
+            .contains('Parameters')
+            .should('be.visible');
+
+        cy.get('p.mb-2')
+            .first() // Get the first matching element
+            .should('be.visible')
+            .within(() => {
+                cy.get('strong.text-dark')
+                    .should('contain.text', 'url')
+                    .and('be.visible');
+
+                cy.get('span.font-weight-bold')
+                    .should('contain.text', '(required)')
+                    .and('be.visible');
+            });
+
+        // Check if the 'code-snippet' exists
+        cy.get('#endpointCollapse1 > .card-body > .fluid.mt-3')
+            .should('exist') // Verify it's present in the DOM
+            .and('be.visible'); // Verify it's visible on the page
+
+        // Check if the 2nd 'code-box' exists
+        cy.get('#endpointCollapse1 > .card-body > .code-response')
+            .should('exist') // Verify it's present
+            .and('be.visible'); // Verify it's visible
+
+        // Verify the Subscribe button is visible
+        cy.get('#subscribeButton')
+            .should('be.visible')
+            .and('contain.text', 'Subscribe for Free')
+            .click();
+
+        // Check if the pricing tab is active after clicking the button
+        cy.get('a#pricing-tab')
+            .should('have.class', 'active')
+            .and('be.visible');
+
+        cy.get('.d-md-flex > .d-none').contains('Documentation').click();
+    });
+
+    it('15. Verify the "POST /upload" endpoint content and click "Subscribe for Free"', () => {
+
+        // Verify the POST /upload heading is present and visible
+        cy.get('#endpointHeading2 button')
+            .find('i.fas.fa-angle-right')
+            .should('be.visible');
+
+        // Verify Parameters section header
+        cy.get('#endpointCollapse2 .card-body h6')
+            .contains('Parameters')
+            .should('be.visible');
+
+        // Verify 'body' parameter section
+        cy.get('p.mb-2')
+            .first() // Get the first matching element
+            .should('be.visible')
+            .within(() => {
+                cy.get('strong.text-dark')
+                    .should('contain.text', 'url')
+                    .and('be.visible');
+
+                cy.get('span.font-weight-bold')
+                    .should('contain.text', '(required)')
+                    .and('be.visible');
+            });
+
+        // Check if the 'code-snippet' exists
+        cy.get('#endpointCollapse1 > .card-body > .fluid.mt-3')
+            .should('exist') // Verify it's present in the DOM
+            .and('be.visible'); // Verify it's visible on the page
+
+        // Check if the 2nd 'code-box' exists
+        cy.get('#endpointCollapse1 > .card-body > .code-response')
+            .should('exist') // Verify it's present
+            .and('be.visible'); // Verify it's visible
+
+        // Verify and click the 'Subscribe for Free' button
+        cy.get('#subscribeButton')
+            .should('be.visible')
+            .and('contain.text', 'Subscribe for Free')
+            .click();
+
+        // Verify that clicking the button activates the pricing tab
+        cy.get('a#pricing-tab')
+            .should('have.class', 'active')
+            .and('be.visible');
+
+        // Verify Documentation link and click it
+        cy.get('.d-md-flex > .d-none').contains('Documentation').click();
+    });
+});
