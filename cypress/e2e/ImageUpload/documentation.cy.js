6661a802f22dc561460c54b1e945ebafcdc70cf5
AngadCel <105632526+AngadCel@users.noreply.github.com>
2024-10-17 16:23:46 +0530

    resolved comments
    resolved comments

--- cypress/e2e/ImageUpload/documentation.cy.js
+++ cypress/e2e/ImageUpload/documentation.cy.js
@@ -1,4 +1,4 @@
-import { IMAGE_UPLOAD_BASE_URL } from '../../resources/data';
+import { IMAGE_UPLOAD_BASE_URL, DEVELOPMENT_QUICKSTART_GUID } from '../../resources/data';
 
 describe('Image Upload Documentation page', () => {
     before(() => {
@@ -19,30 +19,54 @@ describe('Image Upload Documentation page', () => {
     it('3. Test to click on "development quickstart guide" link', () => {
         cy.contains('a', 'development quickstart guide').should('be.visible');
         cy.contains('a', 'development quickstart guide').click();
+
+        cy.intercept('GET', DEVELOPMENT_QUICKSTART_GUID).as('quickstartRequest');
+
+        // Click the link and wait for the request
+        cy.get(`a[href="${DEVELOPMENT_QUICKSTART_GUID}"]`).invoke('removeAttr', 'target').click();
+        cy.wait('@quickstartRequest');
+
+        cy.get('h1.h2') // Select the <h1> element with the class 'h2'
+            .should('be.visible') // Assert that it is visible
+            .and('contain.text', 'Getting Started'); // Assert that it contains the text "Getting Started"
+
         cy.navigateUrlwithCookies(IMAGE_UPLOAD_BASE_URL);
         cy.get('#documentation-tab').click();
     });
 
     it('4. Test to click on "Accounts Pages" link', () => {
-        cy.contains('a', 'Accounts page').should('be.visible');
-        cy.contains('a', 'Accounts page').click();
+        // Click the "Accounts page" link, ensuring it opens in the same tab
+        cy.contains('a', 'Accounts page').invoke('removeAttr', 'target').click();
+
+        // Check if the "API Marketplace" header is visible
+        cy.get('h1 a.text-dark')
+            .should('be.visible')
+            .and('contain.text', 'API Marketplace');
+
+        // Check if the introductory paragraph is present
+        cy.get('p').should('be.visible')
+            .and('contain.text', 'Discover, integrate, and empower your applications with our API marketplace.');
+
+        // Navigate back to the main page with cookies preserved
         cy.navigateUrlwithCookies(IMAGE_UPLOAD_BASE_URL);
-        cy.get('#documentation-tab').click();
-     });
-    
-     it('5. Test to check "Authentication" hyperlink is present', () => {
+
+        // Perform further actions on the main page
+        cy.contains('span', 'Documentation').should('be.visible').click();
+    });
+
+    it('5. Test to check "Authentication" hyperlink is present', () => {
         cy.contains('a', 'Authentication');
     });
-  
+
     it('6. Test to click on "Authentication" hyperlink and check "Authentication" title is present', () => {
         cy.contains('a', 'Authentication').click();
         cy.checkTextVisibility('Authentication');
     });
-  
+
     it('7. Test to check "Endpoints" hyperlink is present', () => {
         cy.contains('a', 'Endpoints');
     });
-  
+
     it('8. Test to click on "Endpoints" hyperlink and check "Endpoints" title is present', () => {
         cy.contains('a', 'Endpoints').click();
         cy.checkTextVisibility('Endpoints');
@@ -57,16 +81,16 @@ describe('Image Upload Documentation page', () => {
     it('10. Test to check "Rate Limiting" hyperlink is present', () => {
         cy.contains('a', 'Rate Limiting');
     });
-  
+
     it('11. Test to click on "Rate Limiting" hyperlink and check "Rate Limiting" title is present', () => {
         cy.contains('a', 'Rate Limiting').click();
         cy.checkTextVisibility('Rate Limiting');
     });
-  
+
     it('12. Test to check "Error Codes" hyperlink is present', () => {
         cy.contains('a', 'Error Codes');
     });
-  
+
     it('13. Test to click on "Error Codes" hyperlink and check "Error Codes" title is present', () => {
         cy.contains('a', 'Error Codes').click();
         cy.checkTextVisibility('Error Codes');
@@ -81,9 +105,15 @@ describe('Image Upload Documentation page', () => {
 
     it('15. Test to click on "POST/upload" collapse button ', () => {
         cy.get('#endpointHeading1 > .mb-0 > .btn').click();
-     });
+        cy.get('.card-btn-toggle-default')
+            .should('be.visible')
+            .and('contain.text', '+');
+    });
 
-     it('16. Test to click on "POST/url" collapse button ', () => {
+    it('16. Test to click on "POST/url" collapse button ', () => {
         cy.get('#endpointHeading2 > .mb-0 > .btn').click();
-     });
+        cy.get('.card-btn-toggle-default')
+            .should('be.visible')
+            .and('contain.text', '+');
+    });
 });
